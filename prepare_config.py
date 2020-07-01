9ac25c365dae941ec94b54ad4837fc03ce11fbc8
gaowei <gaowei0518@gmail.com>
2020-07-01 21:48:03 +0900

    Update prepare_config.py

--- prepare_config.py
+++ prepare_config.py
@@ -8,8 +8,8 @@ def prepare_config_file(model_name , output_file_path = "" ,batch_size = 16 , su
         output_file_path = os.path.join(os.environ["YOLO_DARKNET"],"cfg",model_name + ".cfg")
     if max_iter == 0 :
         max_iter = max(nb_image,2000 * nb_class)
-    90max_iter = int(0.9*max_iter)
-    80max_iter = int(0.8*max_iter)
+    max_iter90 = int(0.9*max_iter)
+    max_iter80 = int(0.8*max_iter)
     if subdivision == 0 :
         subdivision = max(1,int(batch_size//4))
         
@@ -24,8 +24,8 @@ def prepare_config_file(model_name , output_file_path = "" ,batch_size = 16 , su
     new_text = new_text.replace("$SUBDIVISIONS$",str(subdivision))
     new_text = new_text.replace("$ACTIVE_MOSAIC$",str(mosaic))
     new_text = new_text.replace("$MAX_ITERS$",str(max_iter))
-    new_text = new_text.replace("$0.8_ITERS$",str(80max_iter))
-    new_text = new_text.replace("$0.9_ITERS$",str(90max_iter))
+    new_text = new_text.replace("$0.8_ITERS$",str(max_iter90))
+    new_text = new_text.replace("$0.9_ITERS$",str(max_iter80))
     new_text = new_text.replace("$NB_CLASS$",str(nb_class))
     new_text = new_text.replace("$NB_filter$",str(nb_filter))
     
