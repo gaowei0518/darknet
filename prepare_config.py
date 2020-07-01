c8f9e76e80cfb6bf1f9c6f2ce77d348a8c5e61fa
gaowei <gaowei0518@gmail.com>
2020-07-01 18:08:24 +0900

    Update prepare_config.py

--- prepare_config.py
+++ prepare_config.py
@@ -1,6 +1,6 @@
 import os
 
-def prepare_config_file(model_name , output_file_path = "" ,batch_size = 16 , subdivision= 0 , nb_class = 1 , max_iter = 0 , nb_image = 0 , verbose = False) :
+def prepare_config_file(model_name , output_file_path = "" ,batch_size = 16 , subdivision= 0 , nb_class = 1 , max_iter = 0 , nb_image = 0 , mosaic = 1 ,verbose = False) :
     config_sample = os.path.join(os.environ["YOLO_DARKNET"],"cfg","yolov4_sample.cfg")
     
     ### definir les valeur du parmetre : 
@@ -12,6 +12,8 @@ def prepare_config_file(model_name , output_file_path = "" ,batch_size = 16 , su
     80max_iter = int(0.8*max_iter)
     if subdivision == 0 :
         subdivision = max(1,int(batch_size//4))
+        
+    mosaic = bool(mosaic)
     ### regle defini par la reseau , ne le change pas
     nb_filter = (5 + nb_class) *3
     
@@ -20,11 +22,13 @@ def prepare_config_file(model_name , output_file_path = "" ,batch_size = 16 , su
     txt_origin.close()
     new_text = text_origin.replace("$BATCH_SIZE$",str(batch_size))
     new_text = new_text.replace("$SUBDIVISIONS$",str(subdivision))
-    new_text = new_text.replace("$NB_CLASS$",str(nb_class))
-    new_text = new_text.replace("$NB_filter$",str(nb_filter))
+    new_text = new_text.replace("$ACTIVE_MOSAIC$",str(mosaic))
     new_text = new_text.replace("$MAX_ITERS$",str(max_iter))
     new_text = new_text.replace("$0.8_ITERS$",str(80max_iter))
     new_text = new_text.replace("$0.9_ITERS$",str(90max_iter))
+    new_text = new_text.replace("$NB_CLASS$",str(nb_class))
+    new_text = new_text.replace("$NB_filter$",str(nb_filter))
+    
     
     new_cfg = open(output_file_path,"w")
     new_cfg.write(new_text)
